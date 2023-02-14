643a0e9b0598787dd3084ee789a024ea5e4c290b
Matheus Alexandrino <matwsalmeida@gmail.com>
2023-02-14 01:08:43 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const host = 'http://localhost';
+const port = 3030;
+const stats = require('./pcRamUsage.js');
+
+//console.log(http);
+http.createServer((req, res) => {
+    let url = req.url;
+
+    if (url === '/stats') {
+
+        res.end(JSON.stringify(stats, null, 2))
+    } else {
+        res.end('<h1> Seja bem vindo! </h1>')
+    }
+}).listen(port, () => console.log(`Server is Running in  ${host}:${port}, ${stats}`));
